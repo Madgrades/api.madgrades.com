cd7dbed2ded841d6d5f0cfdd0d9da9655b0cd5cf
Keenan Thompson <keenan@keenant.com>
2018-02-28 15:29:27 -0500

    Remove app codes/secrets
    They are invalid anyways

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,15 +2,15 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-           '296091512443-qv3h8f15emcch9m83nbdc3l5qv716nae.apps.googleusercontent.com',
-           'enpzfTWOX2uQ5tv5DKK4z9qg',
+           ENV['GOOGLE_KEY'],
+           ENV['GOOGLE_SECRET'],
            {
                prompt: 'select_account'
            }
 
   provider :github,
-           'b989e6efaa5d026d1beb',
-           '5c35717a1fd3e4cbe5bf1400b964742e3f912736',
+           ENV['GITHUB_KEY'],
+           ENV['GITHUB_SECRET'],
            {
                prompt: 'select_account'
            }
