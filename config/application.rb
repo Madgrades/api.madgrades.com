ff5d72208d1d33923a939fb180fa2620d80933ea
Keenan Thompson <keenan@keenant.com>
2018-04-05 11:04:58 -0500

    Reduce limit to 150/min

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MadGrades
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.middleware.use Rack::Throttle::Minute, :max => 300
+    config.middleware.use Rack::Throttle::Minute, :max => 150
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
